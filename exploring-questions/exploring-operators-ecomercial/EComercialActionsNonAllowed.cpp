e07671bfb6ba6f31599cdc88cb35214452b92d2f
Danilo-dps <danilodpsnilo@outlook.com>
2025-11-03 01:38:30 -0300

    feat: syntax in examples

---
+++ exploring-questions/exploring-operators-ecomercial/EComercialActionsNonAllowed.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+using std::cout;
+using std::endl;
+
+int main(){
+
+    int a = 10;
+    int& aRef = a; 
+
+    //int aRef = &a; errado: int& aRef = &a; 
+    //um valor de tipo "int *" não pode ser utilizado para inicializar uma entidade do tipo "int"
+
+    //aRef = new int(5000); um valor do tipo "int *" não pode ser atribuído para uma entidade do tipo "int"
+    // Em outras palavras, referências não podem ser "rebinded(religada)" para referenciar outros objetos.
+    
+}
